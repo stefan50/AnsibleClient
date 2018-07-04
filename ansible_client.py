9adbef6d9144b9de92b6c4642e4a905b5d32fd1b
stefan50 <ste.angelov.tues19@gmail.com>
2018-07-04 14:48:19 +0300

    Added a method start_connection which almost works. Tested on a virtual machine and all other methods work efficiently

--- ansible_client.py
+++ ansible_client.py
@@ -3,28 +3,30 @@ import sys, os
 class AnsibleClient:
     """
     initializing the class TODO!!!
+    name - name of the connection, "connection" by default
     """
-    def __init__(self):
+    def __init__(self, name="connection"):
         print("Welcome to Ansible Client! What do you want to setup?")
+        self.initialize_file(name, self.parse_hosts())
 
     def parse_hosts(self):
-        res = ""
+        res = []
         self.show_my_hosts("tmp")
         file = open("tmp.txt", "r")
         for line in file:
             if line[0] == "[":
                 # group!!!
-            elif line[0] != "#":
-                res += line
+                pass
+            elif line[0] != "#" and line[0] != "\n":
+                res.append(line)
         file.close()
-        res = res.strip()
         os.system("rm tmp.txt")
         return res
 
 
     def connect_with_hosts(self):
         hosts = self.parse_hosts()
-        os.spawnlp(os.P_WAIT, "ansible", "ansible", hosts, "-m", "ping")
+        os.spawnlp(os.P_WAIT, "ansible", "ansible", hosts, "-m", "ping", "-u", "test")
 
     """
     a method which shows every host - for now a simple cat
@@ -39,8 +41,15 @@ class AnsibleClient:
     Starts the connection
     """
     def start_connection(self):
-        pass
+        os.system("ansible-playbook connection.yml")
 
+    def initialize_file(self, name, hosts):
+        name += ".yml"
+        f = open(name, "w")
+        for h in hosts:
+            f.write("- hosts: " + h)
+        cmd = "cat " + name
+        os.system(cmd)
 
 #p = AnsibleClient()
 #p.show_my_hosts()
--- using_my_api.py
+++ using_my_api.py
@@ -1,4 +1,4 @@
 import ansible_client as ac
 
 p = ac.AnsibleClient()
-p.connect_with_hosts()
+p.start_connection()
