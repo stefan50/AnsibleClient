bff5ff402ec22f497172da555a77f1791991fb2f
stefan50 <ste.angelov.tues19@gmail.com>
2018-07-11 14:41:39 +0300

    Started working more on the high-level file

--- using_my_api.py
+++ using_my_api.py
@@ -1,13 +1,9 @@
 import ansible_client as ac
 
 p = ac.AnsibleClient(user="test")
-#print("Do you want apache2? [N/y]:", end="")
-answer = input("Do you want apache2? [N/y]: ")
-if(answer == 'y'):
-    p.install("192.168.122.148", "apache2")
-answer = input("Do you want nginx? [N/y]: ")
-if(answer == 'y'):
-    p.install("192.168.122.148", "nginx")
 
-#p.install("192.168.122.148", "htop")
-#p.install("192.168.122.148", "apache2")
+apps = ["apache2", "nginx", "htop"]
+for a in apps:
+    answer = input(("Do you want " + a + " [N/y]"))
+    if answer.lower() == 'y' or answer.lower() == 'yes':
+        p.install("192.168.122.148", a)
