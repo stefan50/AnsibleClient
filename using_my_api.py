ae0377ece15f95a0f9f1df76ac78550df750c4f3
stefan50 <ste.angelov.tues19@gmail.com>
2018-07-12 15:25:08 +0300

    Solved an issue where the answer was not registered properly

--- using_my_api.py
+++ using_my_api.py
@@ -2,7 +2,6 @@ import ansible_client as ac
 import sys
 
 answer = input("Do you want a VM? [N/y]:")
-print(answer)
 if answer.lower() == 'y' or answer.lower() == 'yes':
     iso = input("Give me a path to the iso file:")
     name = input("Give me a name for the VM, leave blank for default:")
@@ -15,18 +14,19 @@ del answer
 while(True):
     print("You can do something with your VMs:")
     name = input("Name?:")
+    print(name)
     print("1 - Start a machine")
     print("2 - Stop a machine")
     print("3 - Shut a machine down")
     print("4 - Install")
     answer = input("Now you can choose:")
-    if(answer == 1):
+    if(answer == "1"):
         ac.AnsibleClient.vm_start(name)
-    elif(answer == 2):
+    elif(answer == "2"):
         ac.AnsibleClient.vm_stop(name)
-    elif(answer == 3):
+    elif(answer == "3"):
         ac.AnsibleClient.vm_shutdown(name)
-    elif(answer == 4):
+    elif(answer == "4"):
         p = ac.AnsibleClient(user="test")
         apps = ["apache2", "nginx", "htop"]
         for a in apps:
